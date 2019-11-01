d7f709e9a867ec30518738c099e02cd1d12b276c
BenMaxGCU <bmaxwe200@caledonian.ac.uk>
2019-11-01 13:16:52 +0000

    Added Comment Lines to Java File

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,14 +1,19 @@
+// Imports Java Scanner to read user input
 import java.util.Scanner;
 
+// Class Declaration
 class Dec2Hex
 {
-
+    // Global Variable Declaration for integer Arg1
     public static int Arg1;
 
+    // Main class
     public static void main(String args[])
     {
+        // If statement to check if the value of args is greater than 0
         if (args.length > 0) 
         {
+            // Try/Catch encapsulement to prevent errors
             try 
             {
                 Arg1 = Integer.parseInt(args[0]);
@@ -20,12 +25,14 @@ class Dec2Hex
             }
         }
 
+        // Char declaration as an array with values
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
          int rem, num;
 	num = Arg1;
 	String hexadecimal=""; 
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
         
+        // While num doesn't equal 0 then the loop will continue
         while(num != 0)
         {
 	    rem=num%16;
@@ -33,6 +40,7 @@ class Dec2Hex
             num= num/16;
         }
         
+        // Prints the result in a Hexadecimal format
         System.out.println("Hexadecimal representation is : " + hexadecimal);
 
     }
