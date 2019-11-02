a388b7ef20ab3f2c9c12a4266bf616671b91fad5
BenMaxGCU <bmaxwe200@caledonian.ac.uk>
2019-11-02 17:13:08 +0000

    Added SonarQube Server properties

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -28,9 +28,12 @@ class Dec2Hex
         // Char declaration as an array with values
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
          int rem, num;
-	num = Arg1;
-	String hexadecimal=""; 
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+         
+         num = Arg1;
+         
+         String hexadecimal=""; 
+
+         System.out.println("Converting the Decimal Value " + num + " to Hex...");
         
         // While num doesn't equal 0 then the loop will continue
         while(num != 0)
